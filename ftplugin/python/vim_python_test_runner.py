caa3be924296803b5d6c8ee214ce27d074801244
Dan Berglund <dan@narrativeteam.com>
2014-07-17 14:31:28 +0000

    Split into classes, add functionallity to show nosetests result inline, use tmux

--- ftplugin/python/vim_python_test_runner.py
+++ ftplugin/python/vim_python_test_runner.py
@@ -1,169 +1,90 @@
 #!/bin/env python
-import os
 import re
-import json
+from os.path import splitext
+from xml.etree import ElementTree
 
 
-def get_command_to_run_the_current_app(current_dir):
-    path_to_manage = find_path_to_file(current_dir, "manage.py")
-    if not path_to_manage:
-        return "Not Django"
+class TestCase(object):
+    def __init__(self, vim):
+        self.vim = vim
+        self.buffer = vim.current.buffer
+        self.current_line = vim.current.window.cursor[0]
+        manage_py = "/opt/memoto/bump django"
+        self.project_name = "memsite"
+        self.basecommand = manage_py + ' test '
 
-    app_name = get_app_name(current_dir)
-    env_name = get_json_field_from_config_file(current_dir, "environment")
-    failfast = get_json_field_from_config_file(current_dir, "failfast")
-    failfast = set_flag("failfast", failfast)
-    nocapture = get_json_field_from_config_file(current_dir, "nocapture")
-    nocapture = set_flag("nocapture", nocapture)
+    @property
+    def app(self):
+        path = self.buffer.name
+        components = path.split('/')
+        app_index = components.index(self.project_name) + 1
+        return components[app_index]
 
-    if app_name and env_name:
-        command = "{0} {1} test {2}{3}{4}".format(path_to_manage, env_name, failfast, nocapture, app_name)
-        write_test_command_to_cache_file(command)
-        return (command)
-    elif app_name:
-        command = "{0} test {1}{2}{3}".format(path_to_manage, failfast, nocapture, app_name)
-        write_test_command_to_cache_file(command)
-        return (command)
-    else:
-        return ".vim-django does not exist"
+    @property
+    def file(self):
+        path = self.buffer.name
+        app_file = path.split(self.app)[1].lstrip('/')
+        basename = splitext(app_file)[0]
+        return basename.replace('/', '.')
 
+    @property
+    def cls(self):
+        class_regex = re.compile(r"^class (?P<class_name>.+)\(")
+        for line in xrange(self.current_line - 1, -1, -1):
+            if class_regex.search(self.buffer[line]) is not None:
+                class_name = class_regex.search(self.buffer[line])
+                return class_name.group(1)
 
-def get_command_to_run_the_current_file(current_dir):
-    command_to_current_app = get_command_to_run_the_current_app(current_dir)
-    path_to_tests = get_dot_notation_path_to_test(current_dir)
-    file_name = get_file_name(current_dir)
-    if "Not Django" in command_to_current_app:
-        return "Not Django"
-    elif ".vim-django does not exist" in command_to_current_app or not path_to_tests:
-        return ".vim-django does not exist"
-    else:
-        command = command_to_current_app + "." + path_to_tests + "." + file_name
-        write_test_command_to_cache_file(command)
-        return command
+    @property
+    def method(self):
+        class_regex = re.compile(r"^class (?P<class_name>.+)\(")
+        method_regex = re.compile(r"def (?P<method_name>.+)\(")
+        for line in xrange(self.current_line - 1, -1, -1):
+            if class_regex.search(self.buffer[line]) is not None:
+                return None
+            if method_regex.search(self.buffer[line]) is not None:
+                method_name = method_regex.search(self.buffer[line])
+                return method_name.group(1)
 
-
-def get_command_to_run_the_current_class(current_dir, current_line, current_buffer):
-    class_name = get_current_class(current_line, current_buffer)
-    command_to_current_file = get_command_to_run_the_current_app(current_dir)
-    if "Not Django" in command_to_current_file:
-        return command_to_current_file
-    elif ".vim-django does not exist" in command_to_current_file:
-        return ".vim-django does not exist"
-    else:
-        command = get_command_to_run_the_current_file(current_dir) + ":" + class_name
-        write_test_command_to_cache_file(command)
-        return command
-
-
-def get_command_to_run_the_current_method(current_dir, current_line, current_buffer):
-    method_name = get_current_method(current_line, current_buffer)
-    command_to_current_class = get_command_to_run_the_current_class(current_dir, current_line, current_buffer)
-    if "Not Django" in command_to_current_class:
-        return command_to_current_class
-    elif ".vim-django does not exist" in command_to_current_class:
-        return ".vim-django does not exist"
-    else:
-        command = command_to_current_class + "." + method_name
-        write_test_command_to_cache_file(command)
-        return command
+    def get_command(self, abbr):
+        if abbr != 'rerun':
+            components = [self.project_name]
+            if abbr != 'project':
+                components.append(self.app)
+                if abbr != 'app':
+                    components.append(self.file)
+                    if abbr != 'file':
+                        components.append(self.cls)
+                        if abbr != 'class':
+                            components.append(self.method)
+            command = self.basecommand + '.'.join(components)
+            self.vim.command("let g:last_test_command='{}'".format(command))
+        return self.vim.eval('g:last_test_command')
 
 
-def get_command_to_run_current_file_with_nosetests(path_to_current_file):
-    command = ":!nosetests {0}".format(path_to_current_file)
-    write_test_command_to_cache_file(command)
-    return command
+def ShowTestResults(vim, file):
+    # Define signs
+    vim.command("sign define test_failed text=!! texthl=TestFailure")
+    vim.command("sign define test_success text=() texthl=TestSuccess")
+    vim.command("highlight link TestFailure error")
+    vim.command("highlight TestSuccess ctermbg=Green")
+    tree = ElementTree.parse(file)
+    root = tree.getroot()
+    tests = []
+    for testcase in root.findall('testcase'):
+        cls = testcase.get('classname').rsplit('.')[-1]
+        status = 'success' if testcase.find('failure') is None else 'failed'
+        tests.append({
+            'class': cls,
+            'name': testcase.get('name'),
+            'time': testcase.get('time'),
+            'status': status,
+        })
 
-
-def get_command_to_run_current_class_with_nosetests(path_to_current_file, current_line, current_buffer):
-    run_file = get_command_to_run_current_file_with_nosetests(path_to_current_file)
-    current_class = get_current_class(current_line, current_buffer)
-    command = run_file + ":" + current_class
-    write_test_command_to_cache_file(command)
-    return command
-
-
-def get_command_to_run_current_method_with_nosetests(path_to_current_file, current_line, current_buffer):
-    run_class = get_command_to_run_current_class_with_nosetests(path_to_current_file, current_line, current_buffer)
-    current_method = get_current_method(current_line, current_buffer)
-    command = run_class + "." + current_method
-    write_test_command_to_cache_file(command)
-    return command
-
-
-def get_command_to_rerun_last_tests():
-    with open("/tmp/vim_python_test_runner_cache", "r") as f:
-        return f.read()
-
-
-def write_test_command_to_cache_file(command):
-    with open("/tmp/vim_python_test_runner_cache", "w") as f:
-        f.write(command)
-
-
-def find_path_to_file(current_dir, file_to_look_for):
-    dir_list = [directory for directory in current_dir.split(os.path.sep) if directory]
-    for x in range(len(dir_list) - 1, -1, -1):
-        path_to_check = os.path.sep + os.path.sep.join(dir_list[:x])
-        if file_to_look_for in os.listdir(path_to_check):
-            return path_to_check + os.sep + file_to_look_for
-    return False
-
-
-def get_app_name(current_dir):
-    apps = get_json_field_from_config_file(current_dir, "app_name")
-    try:
-        return [app.lstrip() for app in apps.split(",") if app.lstrip() in current_dir][0]
-    except:
-        return False
-    return False
-
-
-def get_dot_notation_path_to_test(current_dir):
-    app_name = get_app_name(current_dir)
-    if app_name:
-        path_to_tests = current_dir.split(os.sep + app_name + os.sep)[1]
-        return ".".join(path_to_tests.split("/")[:-1])
-    return False
-
-
-def get_file_name(current_dir):
-    path_parts = current_dir.split(os.sep)
-    return path_parts[-1].split(".")[0]
-
-
-def get_current_class(current_line_index, current_buffer):
-    class_regex = re.compile(r"^class (?P<class_name>.+)\(")
-    for line in xrange(current_line_index - 1, -1, -1):
-        if class_regex.search(current_buffer[line]) is not None:
-            class_name = class_regex.search(current_buffer[line])
-            return class_name.group(1)
-    return False
-
-
-def get_current_method(current_line_index, current_buffer):
-    class_regex = re.compile(r"^class (?P<class_name>.+)\(")
-    method_regex = re.compile(r"def (?P<method_name>.+)\(")
-    for line in xrange(current_line_index - 1, -1, -1):
-        if class_regex.search(current_buffer[line]) is not None:
-            return False
-        if method_regex.search(current_buffer[line]) is not None:
-            method_name = method_regex.search(current_buffer[line])
-            return method_name.group(1)
-    return False
-
-
-def get_json_field_from_config_file(current_dir, field_name):
-    try:
-        with open(find_path_to_file(current_dir, ".vim-django"), "r") as f:
-            json_string = f.read()
-        parsed_json = json.loads(json_string)
-        return parsed_json[field_name]
-    except Exception:
-        return False
-
-
-def set_flag(flag, value):
-    if value:
-        return "--{0} ".format(flag)
-    else:
-        return ""
+    for b in vim.buffers:
+        for num, line in enumerate(b[:100], start=1):
+            for test in tests:
+                test_str = "def {}(".format(test['name'])
+                if test_str in line:
+                    vim.command("sign place {} line={} name=test_{} file={}"
+                                .format(num, num, test['status'], b.name))
--- ftplugin/python/vim_python_test_runner.vim
+++ ftplugin/python/vim_python_test_runner.vim
@@ -16,56 +16,41 @@ function! RunDesiredTests(command_to_run)
 python << endPython
 import os
 from sys import platform as _platform
-from vim_python_test_runner import *
-
-def get_proper_command(desired_command, current_directory):
-    current_line_index = vim.current.window.cursor[0]
-    FUNCTIONS = {
-        "django_app": lambda: get_command_to_run_the_current_app(current_directory),
-        "django_file": lambda: get_command_to_run_the_current_file(current_directory),
-        "django_class": lambda: get_command_to_run_the_current_class(current_directory, current_line_index, vim.current.buffer),
-        "django_method": lambda: get_command_to_run_the_current_method(current_directory, current_line_index, vim.current.buffer),
-        "nose_file": lambda: get_command_to_run_current_file_with_nosetests(vim.current.buffer.name),
-        "nose_class": lambda: get_command_to_run_current_class_with_nosetests(vim.current.buffer.name, current_line_index, vim.current.buffer),
-        "nose_method": lambda: get_command_to_run_current_method_with_nosetests(vim.current.buffer.name, current_line_index, vim.current.buffer),
-        "rerun": lambda: get_command_to_rerun_last_tests()
-    }
-    return FUNCTIONS[desired_command]()
-
-def no_errors(command_to_run):
-    if ".vim-django does not exist" == command_to_run:
-        print(".vim-django file does not exist or is improperly formated. ':help run-django-tests'")
-        return False
-    elif "Not Django" == command_to_run:
-        print("Are you sure this is a Django project?")
-        return False
-    return True
+from vim_python_test_runner import TestCase
 
 def run_desired_command_for_os(command_to_run):
-    if "nose" in vim.eval("a:command_to_run") or "nose" in command_to_run:
-        vim.command("{0} 2>&1 | tee /tmp/test_results.txt".format(command_to_run))
-    elif _platform == 'linux' or _platform == 'linux2':
-        vim.command(":!python {0} 2>&1 | tee /tmp/test_results.txt".format(command_to_run))
-    elif _platform == 'darwin':
-        vim.command(":!sudo python {0} 2>&1 | tee /tmp/test_results.txt".format(command_to_run))
+    if _platform == 'linux' or _platform == 'linux2':
+        #vim.command(":Dispatch {0} 2>&1".format(command_to_run))
+        vim.command(":call SendToTmux('{0}\n')".format(command_to_run))
 
 def main():
-    current_directory = os.sep.join([dir for dir in vim.current.buffer.name.split(os.sep) if dir])
-    command_to_run = get_proper_command(vim.eval("a:command_to_run"), current_directory)
-    if no_errors(command_to_run):
-        run_desired_command_for_os(command_to_run)
-        vim.command('silent make!')
+    testcase = TestCase(vim)
+    command_to_run = testcase.get_command(vim.eval("a:command_to_run"))
+    run_desired_command_for_os(command_to_run)
 
 vim.command('wall')
 main()
 endPython
 endfunction
 
-command! DjangoTestApp call RunDesiredTests("django_app")
-command! DjangoTestFile call RunDesiredTests("django_file")
-command! DjangoTestClass call RunDesiredTests("django_class")
-command! DjangoTestMethod call RunDesiredTests("django_method")
-command! NosetestFile call RunDesiredTests("nose_file")
-command! NosetestClass call RunDesiredTests("nose_class")
-command! NosetestMethod call RunDesiredTests("nose_method")
-command! RerunLastTests call RunDesiredTests("rerun")
+function! ShowTestResults()
+python << endPython
+from vim_python_test_runner import ShowTestResults
+ShowTestResults(vim, '/tmp/nosetests.xml')
+endPython
+endfunction
+
+command! DjangoTestApp call RunDesiredTests("app")
+command! DjangoTestClass call RunDesiredTests("class")
+command! DjangoTestMethod call RunDesiredTests("method")
+command! DjangoTestProject call RunDesiredTests("project")
+command! DjangoTestRerun call RunDesiredTests("rerun")
+command! ShowTestResults call ShowTestResults()
+
+nnoremap <Leader>t :DjangoTestMethod<cr>
+nnoremap <Leader>c :DjangoTestClass<cr>
+nnoremap <Leader>a :DjangoTestApp<cr>
+nnoremap <Leader>* :DjangoTestProject<cr>
+nnoremap <Leader>r :DjangoTestRerun<cr>
+
+nnoremap <Leader>s :ShowTestResults<cr>
